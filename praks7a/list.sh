db5b95380a43d5560c08c1115da366bd7a7e3f66
Vallo Ainsar <vallo.ainsar@khk.ee>
2019-05-14 17:12:04 +0300

    kataloogi info

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 #
-ls . -la
+echo -n "sisestage kataloogi nimi mille kohta soovite informatsiooni (täistee): ";read kataloog
+nimi=$(ls -la $kataloog | awk '{if(NR>1)print $9}')
+ft=$(ls -la $kataloog | awk '{if(NR>1)print substr ($0, 0, 2)}')
+number=1
+fail=0
+kaust=0
+link=0
+for type in $ft
+do
+	if [ $type == '-' ]
+	then
+		echo -n "fail: "
+		let fail++
+	elif [ $type == 'd' ]
+	then
+		echo -n "kaust: "
+		let kaust++
+	elif [ $type == 'l']
+	then
+		echo -n "link: "
+		let link++
+	fi
+	echo $nimi | cut -d ' ' -f$number
+	let number++
+done
+echo "Selles kaustas on $fail faili, $kaust kausta ja $link linki..."
