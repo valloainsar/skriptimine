2a3b947180d6f83f40b44f05d551840d0f8fdfdf
valloainsar <48349622+valloainsar@users.noreply.github.com>
2019-05-25 12:22:54 +0300

    Update list.sh

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 #
-echo -n "sisestage kataloogi nimi mille kohta soovite informatsiooni (täistee): ";read kataloog
-nimi=$(ls -la $kataloog | awk '{if(NR>1)print $9}')
-ft=$(ls -la $kataloog | awk '{if(NR>1)print substr ($0, 0, 2)}')
+echo -n "sisestage kataloogi nimi mille kohta soovite informatsiooni (täistee): ";read kataloog # loetakse sisend
+nimi=$(ls -la $kataloog | awk '{if(NR>1)print $9}')	# siin on defineeritud väärtused, mida edaspidi kasutatakse
+ft=$(ls -la $kataloog | awk '{if(NR>1)print substr ($0, 0, 2)}') # defineeritud failitüüp
 number=1
 fail=0
 kaust=0
 link=0
-for type in $ft
+for type in $ft	#läbi lastakse "ft"
 do
-	if [ $type == '-' ]
+	if [ $type == '-' ]	# kui ft on "-", siis...
 	then
-		echo -n "fail: "
-		let fail++
-	elif [ $type == 'd' ]
+		echo -n "fail: "	# on tegu failiga
+		let fail++		# ja lisatakse failile +1 ### Üleval on näha et esialgu on faili väärtus 0, niisamuti ka teistel ###
+	elif [ $type == 'd' ]	# kui ft on "d", siis...
 	then
-		echo -n "kaust: "
+		echo -n "kaust: "	# on tegu kataloogiga
 		let kaust++
-	elif [ $type == 'l']
+	elif [ $type == 'l']	# kui ft on "l" siis...
 	then
-		echo -n "link: "
-		let link++
+		echo -n "link: "	# on tegu lingiga
+		let link++	# lisatakse linglie +1
 	fi
-	echo $nimi | cut -d ' ' -f$number
+	echo $nimi | cut -d ' ' -f $number
 	let number++
 done
 echo "Selles kaustas on $fail faili, $kaust kausta ja $link linki..."
